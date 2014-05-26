273ab8e7a08cb1c7fc9518defaf7bd8683c67349
Dougal Graham <dougalg@gmail.com>
2014-05-26 17:39:07 +0700

    Update Mac app

--- build/LL_OR_BNC_mac.app/Contents/Resources/__boot__.py
+++ build/LL_OR_BNC_mac.app/Contents/Resources/__boot__.py
@@ -7,32 +7,6 @@ def _reset_sys_path():
 _reset_sys_path()
 
 
-def _site_packages():
-    import site, sys, os
-    paths = []
-    prefixes = [sys.prefix]
-    if sys.exec_prefix != sys.prefix:
-        prefixes.append(sys.exec_prefix)
-    for prefix in prefixes:
-        paths.append(os.path.join(prefix, 'lib', 'python' + sys.version[:3],
-            'site-packages'))
-    if os.path.join('.framework', '') in os.path.join(sys.prefix, ''):
-        home = os.environ.get('HOME')
-        if home:
-            paths.append(os.path.join(home, 'Library', 'Python',
-                sys.version[:3], 'site-packages'))
-
-    # Work around for a misfeature in setuptools: easy_install.pth places
-    # site-packages way to early on sys.path and that breaks py2app bundles.
-    # NOTE: this is hacks into an undocumented feature of setuptools and
-    # might stop to work without warning.
-    sys.__egginsert = len(sys.path)
-
-    for path in paths:
-        site.addsitedir(path)
-_site_packages()
-
-
 """
 sys.argv emulation
 
@@ -313,12 +287,13 @@ def _chdir_resource():
 _chdir_resource()
 
 
-def _path_inject(paths):
-    import sys
-    sys.path[:0] = paths
-
-
-_path_inject(['/Volumes/Cheesedata/workspace/LL_OR_BNC'])
+def _disable_linecache():
+    import linecache
+    def fake_getline(*args, **kwargs):
+        return ''
+    linecache.orig_getline = linecache.getline
+    linecache.getline = fake_getline
+_disable_linecache()
 
 
 import re, sys
@@ -342,11 +317,13 @@ def _run():
     global __file__
     import os, site
     sys.frozen = 'macosx_app'
+    base = os.environ['RESOURCEPATH']
 
     argv0 = os.path.basename(os.environ['ARGVZERO'])
     script = SCRIPT_MAP.get(argv0, DEFAULT_SCRIPT)
 
-    sys.argv[0] = __file__ = script
+    path = os.path.join(base, script)
+    sys.argv[0] = __file__ = path
     if sys.version_info[0] == 2:
         with open(script, 'rU') as fp:
             source = fp.read() + "\n"
@@ -356,13 +333,9 @@ def _run():
 
         with open(script, 'r', encoding=encoding) as fp:
             source = fp.read() + '\n'
+    exec(compile(source, path, 'exec'), globals(), globals())
 
-    exec(compile(source, script, 'exec'), globals(), globals())
 
-
-DEFAULT_SCRIPT='/Volumes/Cheesedata/workspace/LL_OR_BNC/LL_OR_BNC_app.py'
+DEFAULT_SCRIPT='LL_OR_BNC_app.py'
 SCRIPT_MAP={}
-try:
-    _run()
-except KeyboardInterrupt:
-    pass
+_run()
