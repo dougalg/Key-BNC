c0aec343a31afb385ef3212b43c7142e8db84aa4
dougalg <dougalg@gmail.com>
2020-04-10 10:46:52 +0700

    Tabs

--- KEY_BNC/stats/odds_ratio.py
+++ KEY_BNC/stats/odds_ratio.py
@@ -1,41 +1,41 @@
 def OR(
-        target_freq,
-        target_corpus_size,
-        comparison_freq,
-        comparison_corpus_size,
-        zero_adjustment=None
-    ):
-    r"""
-    Based on the definition found at:
-    http://www.medcalc.org/manual/relativerisk_oddsratio.php
+		target_freq,
+		target_corpus_size,
+		comparison_freq,
+		comparison_corpus_size,
+		zero_adjustment=None
+	):
+	r"""
+	Based on the definition found at:
+	http://www.medcalc.org/manual/relativerisk_oddsratio.php
 
-    >>> OR(12,24,12,24)
-    1.0
+	>>> OR(12,24,12,24)
+	1.0
 
-    >>> OR(100, 105, 12, 24)
-    20.0
+	>>> OR(100, 105, 12, 24)
+	20.0
 
-    Division by 0 returns infinity
-    >>> OR(12,12,100,1000)
-    inf
+	Division by 0 returns infinity
+	>>> OR(12,12,100,1000)
+	inf
 
-    Can be ignored with a zero_adjustment value
-    >>> OR(12,12,100,1000, zero_adjustment=0.5)
-    224.00497512437812
+	Can be ignored with a zero_adjustment value
+	>>> OR(12,12,100,1000, zero_adjustment=0.5)
+	224.00497512437812
 
-    >>> OR(1,10000000,10**-9,1000000000000)
-    100000010000000.98
-    """
+	>>> OR(1,10000000,10**-9,1000000000000)
+	100000010000000.98
+	"""
 
-    a = float(target_freq)
-    b = float(target_corpus_size - target_freq)
-    c = float(comparison_freq)
-    d = float(comparison_corpus_size - comparison_freq)
+	a = float(target_freq)
+	b = float(target_corpus_size - target_freq)
+	c = float(comparison_freq)
+	d = float(comparison_corpus_size - comparison_freq)
 
-    if 0 in [b, c, d] and zero_adjustment is not None:
-        a, b, c, d = [i+zero_adjustment for i in [a, b, c, d]]
+	if 0 in [b, c, d] and zero_adjustment is not None:
+		a, b, c, d = [i+zero_adjustment for i in [a, b, c, d]]
 
-    try:
-        return (a/b)/(c/d)
-    except (FloatingPointError, ZeroDivisionError):
-        return float('inf')
+	try:
+		return (a/b)/(c/d)
+	except (FloatingPointError, ZeroDivisionError):
+		return float('inf')
--- generate_BNC_wordlist.py
+++ generate_BNC_wordlist.py
@@ -2,7 +2,7 @@ from KEY_BNC.KEY_BNC import KEY_BNC
 import csv
 
 def print_name(the_name):
-    print(the_name)
+	print(the_name)
 
 target_dir = r'D:\BNC-TXT'
 
@@ -10,8 +10,8 @@ calculator = KEY_BNC()
 calculator.load_target_data_dir(target_dir)
 
 with open('New_BNC_wordlist.csv', mode='w', encoding='utf8', newline='') as outf:
-    writer = csv.writer(outf)
-    i = 0
-    writer.writerow(['Rank', 'Frequency', 'Word'])
-    for rank, fw in enumerate(calculator.target_words.most_common()):
-        writer.writerow([rank, fw[1], fw[0]])
+	writer = csv.writer(outf)
+	i = 0
+	writer.writerow(['Rank', 'Frequency', 'Word'])
+	for rank, fw in enumerate(calculator.target_words.most_common()):
+		writer.writerow([rank, fw[1], fw[0]])
