9a53a0ae9acbeaeaebd4398b9893ba84f322bf07
dougalg <dougalg@gmail.com>
2020-05-05 14:13:10 +0700

    CLI works

--- rust/key_bnc_cli/src/main.rs
+++ rust/key_bnc_cli/src/main.rs
@@ -1,6 +1,7 @@
 mod bnc_data;
 
 use key_bnc_utils::utils::{tokenize, collect};
+use key_bnc_utils::stats::{odds_ratio, log_likelyhood, dispersion_normalized};
 use bnc_data::get_bnc_count;
 use std::path::Path;
 use std::env;
@@ -13,6 +14,12 @@ use counter::Counter;
 fn main() {
 	let counter = 0;
 	let bnc_counts = get_bnc_count();
+	let mut total_num_tokens_in_bnc = 0;
+
+	for (_w, c) in bnc_counts.iter() {
+		total_num_tokens_in_bnc += c;
+	}
+
 	let args: Vec<String> = env::args().collect();
 	let root_dir = &args[1];
 	println!("Reading {:#?}", root_dir);
@@ -39,30 +46,54 @@ fn main() {
 		all_words_counts += entry.word_counts.clone();
 	}
 
+	let entries: Vec<CorpusPart> = entries.into_iter()
+		.map(|entry| CorpusPart {
+			id: entry.id,
+			name: entry.name,
+			percent_of_total: entry.token_count as f64 / total_num_tokens as f64,
+			token_count: entry.token_count,
+			word_counts: entry.word_counts,
+		})
+		.collect();
+
 	let results: Vec<WordStats> = all_words_counts
 		.into_map()
 		.into_iter()
 		.map(|(word, count)| {
+			let count_in_bnc = match bnc_counts.get(&word) {
+				Some(val) => *val as f64,
+				None => 0.0,
+			};
+			// [(part_percentage, part_freq)]
+			let part_data: Vec<(f64, f64)> = entries.iter()
+				.map(|entry| {
+					let count_for_part = match entry.word_counts.get(&word) {
+						Some(val) => *val as f64,
+						None => 0.0,
+					};
+					(entry.percent_of_total, count_for_part)
+				})
+				.collect();
 			WordStats {
 				count,
 				word,
-				ll: 0.0,
-				or: 0.0,
-				dispersion: 0.0,
+				log_likelyhood: log_likelyhood(count as f64, total_num_tokens as f64, count_in_bnc as f64, total_num_tokens_in_bnc as f64),
+				odds_ratio: odds_ratio(count as f64, total_num_tokens as f64, count_in_bnc as f64, total_num_tokens_in_bnc as f64, 0.0),
+				dispersion: dispersion_normalized(&part_data, count as f64),
 			}
 		})
 		.collect();
 
-	println!("{:#?}", results);
-	println!("{:#?}", total_num_tokens);
-	println!("{:#?}", bnc_counts.len());
+	// println!("{:#?}", results);
+	// println!("{:#?}", total_num_tokens);
+	// println!("{:#?}", bnc_counts.len());
 }
 
 #[derive(Debug)]
 pub struct CorpusPart {
 	id: i32,
 	name: String,
-	percent_of_total: f32,
+	percent_of_total: f64,
 	token_count: usize,
 	word_counts: Counter<UniCase<String>>,
 }
@@ -71,9 +102,9 @@ pub struct CorpusPart {
 pub struct WordStats {
 	word: UniCase<String>,
 	count: usize,
-	ll: f32,
-	or: f32,
-	dispersion: f32,
+	log_likelyhood: f64,
+	odds_ratio: f64,
+	dispersion: f64,
 }
 
 fn process_file(entry: &DirEntry, id: i32) -> CorpusPart {
