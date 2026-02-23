9849512ffc2ba16c774dce7c833f256f29fb11d1
Dougal Graham <dougalg@gmail.com>
2026-02-23 22:57:23 +0700

    Update to latest rust

--- key_bnc_web/key_bnc_wasm/src/lib.rs
+++ key_bnc_web/key_bnc_wasm/src/lib.rs
@@ -10,12 +10,6 @@ use unicase::UniCase;
 use wasm_bindgen::prelude::*;
 use web_sys::FileReader;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 #[derive(Debug)]
 pub struct CorpusPart {
     token_count: usize,
